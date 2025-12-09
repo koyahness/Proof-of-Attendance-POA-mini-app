551e1befae71d8806176472fc637d15d3ef778d9
koyahness <cryptomakoya@gmail.com>
2025-12-09 09:14:15 +0200

    doc updates

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 
 import { MiniAppDemo } from "@/components/cdp-demo/MiniAppDemo";
 
+import { useEffect } from "react"; // <-- **MISSING IMPORT ADDED HERE**
+
 import { useMiniKit } from "@coinbase/onchainkit/minikit";
 
 export default function HomePage() {
